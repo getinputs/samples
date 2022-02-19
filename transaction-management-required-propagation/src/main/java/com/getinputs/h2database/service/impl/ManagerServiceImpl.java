71b856fdc9c18a783e18290dfcea96e84c00d302
getinputs <getinputs.nagrale.suraj@gmail.com>
2022-02-19 20:44:10 +0530

    Sample application on how the spring boot transaction management works with Required Propagation level

---
+++ transaction-management-required-propagation/src/main/java/com/getinputs/h2database/service/impl/ManagerServiceImpl.java
@@ -0,0 +1,30 @@
+package com.getinputs.h2database.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.getinputs.h2database.dao.ManagerDao;
+import com.getinputs.h2database.model.Manager;
+import com.getinputs.h2database.service.ManagerService;
+
+@Service
+public class ManagerServiceImpl implements ManagerService {
+
+	@Autowired
+	private ManagerDao managerDao;
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public String saveManager() {
+		
+		String response = "Manager Saved";
+		
+		Manager manager = new Manager("Suraj");
+		managerDao.save(manager);
+		
+		System.out.println(response);
+		return response;
+	}
+}
